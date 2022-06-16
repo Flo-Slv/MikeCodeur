3553674a1db693a98b5f50724e32db2a336e4f3c
Flo Slv <florent3969@hotmail.fr>
2022-06-16 16:28:07 +0200

    Add react-prerequis-javascript

---
+++ react-prerequis-javascript/src/__tests__/01.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import {robertAlertTip} from '@mikecodeur/react-course-app/test-utils'
+import chalk from 'chalk'
+import {render, screen, prettyDOM} from '@testing-library/react'
+
+//import App from '../final/01'
+//import App from '../exercise/01'
+
+test('Mock', () => {
+  expect(true).toBe(true)
+})
