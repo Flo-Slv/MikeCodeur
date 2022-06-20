2a70369bd6a6eb36763cec9e101fd4809457c249
Flo Slv <florent3969@hotmail.fr>
2022-06-20 14:14:09 +0200

    Exercice 8 done !

--- react-fondamentaux/src/exercise/08.js
+++ react-fondamentaux/src/exercise/08.js
@@ -9,30 +9,38 @@ import * as React from 'react'
 // 🐶 dans la fonction 'addSkillPython' logue le tableau skills
 // 🤖 console.log('skills',skills)
 
-const skills = [
-  {id: 'e313', value: 'HTML'},
-  {id: 'f980', value: 'CSS'},
-  {id: '11eb', value: 'JS'},
-  {id: '9a03', value: 'REACT'},
-  {id: '65d4', value: 'ANGULAR'},
+const initialSkills = [
+	{id: 'e313', value: 'HTML'},
+	{id: 'f980', value: 'CSS'},
+	{id: '11eb', value: 'JS'},
+	{id: '9a03', value: 'REACT'},
+	{id: '65d4', value: 'ANGULAR'},
 ]
 
 // 🐶 Créé un button dans le render et sur l'evenement onclick passer la fonction 'addSkillPython'
 // 🤖 <button onClick={addSkillPython}
-function MesSkills() {
-  return (
-    <>
-      <ul>
-        {skills.map(skill => (
-          <li key={skill.id}>{skill.value}</li>
-        ))}
-      </ul>
-    </>
-  )
+let MesSkills = (props) => {
+	const [skills, setSkills] = React.useState(initialSkills);
+
+	let addSkillPython = () => {
+		// skills.push({ id: `${Math.random()}`, value: 'PYTHON'});
+		setSkills([...skills, { id: `${Math.random()}`, value: 'PYTHON'}]);
+		// console.log('skills: ', skills);
+	};
+
+	return <>
+		<ul>
+		{skills.map(skill => (
+			<li key={skill.id}>{skill.value}</li>
+		))}
+		</ul>
+		<br />
+		<button onClick={addSkillPython}>Add skills</button>
+	</>
 }
 
 function App() {
-  return <MesSkills />
+	return <MesSkills />
 }
 
 export default App
