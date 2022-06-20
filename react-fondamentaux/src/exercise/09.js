465783aafa7f6939ba8e33240711eb5b1d2c7627
Flo Slv <florent3969@hotmail.fr>
2022-06-20 15:06:03 +0200

    Exercice 9 done !

--- react-fondamentaux/src/exercise/09.js
+++ react-fondamentaux/src/exercise/09.js
@@ -1,29 +1,48 @@
 // Les formulaires
-// http://localhost:3000/alone/exercise/08.js
+// http://localhost:3000/alone/exercise/09.js
 
-import * as React from 'react'
+import * as React from 'react';
 
-function LoginForm() {
-  // 🐶 Gère l'événement onSubmit de <form> en créant une fonction 'handleSubmit'
-  // 🤖 <form onSubmit={handleSubmit}>
-  // 🤖 Utilise `event.preventDefault()` dans la fonction handleSubmit pour stopper
-  // le comportement par defaut du formulaire (soumission et rafraichissement page)
+let LoginForm = () => {
+	// 🐶 Gère l'événement onSubmit de <form> en créant une fonction 'handleSubmit'
+	// 🤖 <form onSubmit={handleSubmit}>
+	// 🤖 Utilise `event.preventDefault()` dans la fonction handleSubmit pour stopper
+	// le comportement par defaut du formulaire (soumission et rafraichissement page)
+	const [email, setEmail] = React.useState();
+	const [error, setError] = React.useState();
 
-  // 🐶 Affiche ensuite une popup Bonjour 'email@email.fr'
-  // 🤖 Utilise `event.target.elements.emailInput.value` pour récupèrer la valeur de l'email
-  return (
-    <form>
-      <label>
-        Adresse email :
-        <input type="text" name="emailInput" />
-      </label>
-      <input type="submit" value="Connexion" />
-    </form>
-  )
-}
+	let handleSubmit = event => {
+		event.preventDefault();
+		alert(`Bonjour ${event.target.elements.emailInput.value}`);
+		// alert(`Bonjour ${inputRef.current.value}`);
+	};
 
-function App() {
-  return <LoginForm />
-}
 
-export default App
+	let handleChange = event => {
+		setEmail(event.target.value);
+		setError(event.target.value.includes('@') ? null : "Email non valide !");
+
+	};
+
+	// const inputRef = React.useRef();
+
+	// 🐶 Affiche ensuite une popup Bonjour 'email@email.fr'
+	// 🤖 Utilise `event.target.elements.emailInput.value` pour récupèrer la valeur de l'email
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>
+				Adresse email :
+				<input type="text" name="emailInput" onChange={handleChange} />
+			</label>
+
+			<input type="submit" value="Connexion" />
+			<div style={{color: 'red'}}>{error}</div>
+		</form>
+	);
+};
+
+let App = () => {
+	return <LoginForm />
+};
+
+export default App;
