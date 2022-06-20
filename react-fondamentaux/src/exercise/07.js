88557d156c4679f4178fc3341f58188e9480b0ec
Flo Slv <florent3969@hotmail.fr>
2022-06-20 13:45:21 +0200

    Exercice 7 done !

--- react-fondamentaux/src/exercise/07.js
+++ react-fondamentaux/src/exercise/07.js
@@ -5,16 +5,26 @@ import * as React from 'react'
 
 // 🐶 Le code suivant génère un warning :  Warning: Each child in a list should have a unique "key" prop
 
-const skills = []
+// const skills = ["HTML", "CSS", "JS", "REACT", "ANGULAR"];
+const skills = [
+	{ id: '01', value: "HTML"},
+	{ id: '02', value: "CSS"},
+	{ id: '03', value: "JS"},
+	{ id: '04', value: "REACT"},
+	{ id: '05', value: "ANGULAR"}
+];
+
 // 🐶 Corrige ce code avec des keys uniques sur les elements <li>
-skills.push(<li>HTML</li>)
-skills.push(<li>CSS</li>)
-skills.push(<li>JS</li>)
-skills.push(<li>REACT</li>)
-skills.push(<li>ANGULAR</li>)
+// skills.push(<li key="html">HTML</li>)
+// skills.push(<li key="css">CSS</li>)
+// skills.push(<li key="js">JS</li>)
+// skills.push(<li key="react">REACT</li>)
+// skills.push(<li key="angular">ANGULAR</li>)
 
 function MesSkills() {
-  return <ul>{skills.map(skill => skill)}</ul>
+  return <ul>{skills.map(skill => {
+	  return <li key={skill.id}>{skill.value}</li>
+  })}</ul>
 }
 
 function App() {
