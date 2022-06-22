e679eb611dec9f30713b0a90398054322beee853
Flo Slv <florent3969@hotmail.fr>
2022-06-22 17:06:55 +0200

    Exercice 4 done !

--- react-patterns/src/exercise/04.js
+++ react-patterns/src/exercise/04.js
@@ -1,7 +1,7 @@
 // Props Drilling
 // http://localhost:3000/alone/exercise/04.js
 /* eslint-disable no-unused-vars */
-import * as React from 'react'
+import * as React from 'react';
 
 // 🐶 Fais évoluer le Header en ajoutant en deux composants enfants <Login /> <MenuBarInfo />
 // Login retourne <div>Connexion au site : {siteName} </div>
@@ -9,46 +9,60 @@ import * as React from 'react'
 
 // 🐶 Tu vas devoir faire transiter les props depuis App -> Header -> Login/MenuBarInfo
 
-function Header({siteName}) {
-  return <h1>Bienvenue sur {siteName} </h1>
-}
-function Content({siteName}) {
-  return <div>Article du site {siteName} </div>
-}
-function Footer({siteName}) {
-  return (
-    <>
-      <CGV siteName={siteName} />
-      <About siteName={siteName} />
-    </>
-  )
-}
-function CGV({siteName}) {
-  return <div>CGV du site : {siteName} </div>
-}
+const Login = ({ siteName }) => {
+	return <div>
+		Connexion au site: {siteName}
+	</div>;
+};
 
-function About({siteName}) {
-  // 🐶 Tu vas devoir faire évoluer <About> pour qu'il utilise l'email de App
-  // 🤖 <div>Contactez nous : {email}</div>
-  return (
-    <>
-      <div>A propos du site : {siteName} </div>
-      <div>Contactez nous : contact@mikecodeur.com</div>
-    </>
-  )
-}
+const MenuBarInfo = ({ nbMessages }) => {
+	return <>
+		Vous avez {nbMessages} messages.
+	</>;
+};
 
-function App() {
-  const siteName = 'mikecodeur.com'
-  const email = 'contact@mikecodeur.com'
-  const nbMessages = 18
-  return (
-    <>
-      <Header siteName={siteName} />
-      <Content siteName={siteName} />
-      <Footer siteName={siteName} />
-    </>
-  )
-}
+const Header = ({ siteName, nbMessages }) => {
+	return <>
+		<Login siteName={siteName} />
+		<MenuBarInfo  nbMessages={nbMessages} />
+		<h1>Bienvenue sur {siteName} </h1>
+	</>;
+};
 
-export default App
+const Content = ({siteName}) => {
+	return <div>Article du site {siteName} </div>;
+};
+
+const Footer = ({ siteName, email }) => {
+	return <>
+		<CGV siteName={siteName} />
+		<About siteName={siteName} email={email} />
+	</>;
+};
+
+const CGV = ({siteName}) => {
+	return <div>CGV du site : {siteName} </div>;
+};
+
+const About = ({ siteName, email }) => {
+	// 🐶 Tu vas devoir faire évoluer <About> pour qu'il utilise l'email de App
+	// 🤖 <div>Contactez nous : {email}</div>
+	return <>
+		<div>A propos du site : {siteName} </div>
+		<div>Contactez nous : {email}</div>
+	</>;
+};
+
+const App = () => {
+	const siteName = 'mikecodeur.com';
+	const email = 'contact@mikecodeur.com';
+	const nbMessages = 18;
+
+	return <>
+		<Header siteName={siteName} nbMessages={nbMessages} />
+		<Content siteName={siteName} />
+		<Footer siteName={siteName} email={email}/>
+	</>;
+};
+
+export default App;
