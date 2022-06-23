a67a1eec7b2e6d0448729d60a6e19dd44d6009c8
Flo Slv <florent3969@hotmail.fr>
2022-06-23 10:06:24 +0200

    Exercice 6 done !

--- react-patterns/src/exercise/06.js
+++ react-patterns/src/exercise/06.js
@@ -1,56 +1,57 @@
 // Lifting State Up
 // http://localhost:3000/alone/exercise/06.js
 
-import * as React from 'react'
+import * as React from 'react';
 
 // 🐶 Modifie 'NameInput' et 'FirstNameInput' pour que leurs states soient gérer par le composant parent App.
 
 // 🐶 Créé deux props : onNameChange et name
 // 🤖 function NameInput({onNameChange,name}) {
-function NameInput() {
-  // ⛏️ supprime le state name
-  const [name, setName] = React.useState('')
-  const handleChange = e => {
-    // 🐶 fait appel 'onNameChange' pour faire remonter le state
-    setName(e.target.value)
-  }
-  return (
-    <span>
-      <label>Name : </label>
-      <input type="text" value={name} onChange={handleChange} />
-    </span>
-  )
-}
+const NameInput = ({ onNameChange, name }) => {
+	// ⛏️ supprime le state name
+	// const [name, setName] = React.useState('');
+	const handleChange = e => onNameChange(e.target.value);
+		// 🐶 fait appel 'onNameChange' pour faire remonter le state
+		// setName(e.target.value);
+
+	return <span>
+		<label>Name : </label>
+		<input type="text" value={name} onChange={handleChange} />
+	</span>;
+};
 
 // 🐶 Répète les mêmes étapes que pour 'NameInput'
-function FirstNameInput() {
-  const [firstName, setFirstName] = React.useState('')
-  const handleChange = e => {
-    setFirstName(e.target.value)
-  }
-  return (
-    <span>
-      <label>FirstName : </label>
-      <input type="text" value={firstName} onChange={handleChange} />
-    </span>
-  )
-}
-function App() {
-  // 🐶 Créé deux states 'name' et 'firstName'
-  // 🤖 const [name, setName] = React.useState('')
+const FirstNameInput = ({ onFirstNameChange, firstName }) => {
+	// const [firstName, setFirstName] = React.useState('');
+	const handleChange = e => onFirstNameChange(e.target.value);
+		// setFirstName(e.target.value)
 
-  // 🐶 Créé deux fonctions 'handleChangeName' et 'handleChangeFirstName'
-  // qui prenent un nom en paramètre et met à jour le state
-  // 🤖 const handleNameChange = name => { setName(name) }
+	return <span>
+		<label>FirstName : </label>
+		<input type="text" value={firstName} onChange={handleChange} />
+	</span>;
+};
 
-  return (
-    <div>
-      {/* 🐶 Passe les bons props (fonctions et données)  */}
-      <NameInput />
-      <FirstNameInput />
-      <div>Bonjour </div>
-    </div>
-  )
-}
+const App = () => {
+	// 🐶 Créé deux states 'name' et 'firstName'
+	// 🤖 const [name, setName] = React.useState('');
+	const [name, setName] = React.useState('');
+	const [firstName, setFirstName] = React.useState('');
 
-export default App
+	const handleChangeName = name => setName(name);
+
+	const handleChangeFirstName = firstName => setFirstName(firstName);
+
+	// 🐶 Créé deux fonctions 'handleChangeName' et 'handleChangeFirstName'
+	// qui prenent un nom en paramètre et met à jour le state
+	// 🤖 const handleNameChange = name => { setName(name) }
+
+	return <div>
+		{/* 🐶 Passe les bons props (fonctions et données)  */}
+		<NameInput onNameChange={handleChangeName} name={name} />
+		<FirstNameInput onFirstNameChange={handleChangeFirstName} firstName={firstName} />
+		<div>Bonjour {name} {firstName} </div>
+	</div>;
+};
+
+export default App;
