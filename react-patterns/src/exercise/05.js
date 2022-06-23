0c8cfb7e9f09fd06d47007dc6f03f165b9b078ca
Flo Slv <florent3969@hotmail.fr>
2022-06-23 09:36:04 +0200

    Exercice 5 done !

--- react-patterns/src/exercise/05.js
+++ react-patterns/src/exercise/05.js
@@ -1,48 +1,49 @@
 // State Hoisting
 // http://localhost:3000/alone/exercise/05.js
 
-import * as React from 'react'
-import {fetchTodoAPI} from '../data'
+import * as React from 'react';
+import {fetchTodoAPI} from '../data';
 
 // 🐶 Créé un props 'todos'
-function Header() {
-  //⚠️ Dans la réalité il faudrait utiliser useEffect.
-  // Ici fetchTodoAPI() est synchrone et juste pour la démonstration.
+const Header = ({ todos }) => {
+	//⚠️ Dans la réalité il faudrait utiliser useEffect.
+	// Ici fetchTodoAPI() est synchrone et juste pour la démonstration.
 
-  // ⛏️ Supprime (deplace dans App) l'appel API et le State Todos 
-  // et utilise le props 'todos' à la place
-  const todosFromAPI = fetchTodoAPI()
-  const [todos] = React.useState(todosFromAPI)
-  return <div>Vous avez {todos.length} tâches </div>
-}
+	// ⛏️ Supprime (deplace dans App) l'appel API et le State Todos
+	// et utilise le props 'todos' à la place
+	// const todosFromAPI = fetchTodoAPI();
+	// const [todos] = React.useState(todosFromAPI);
+
+	return <div>Vous avez {todos.length} tâches </div>;
+};
 
 // 🐶 Créé un props 'todos'
-function TodoList() {
-   // ⛏️ Supprime (deplace dans App) l'appel API et le State Todos 
-  // et utilise le props 'todos' à la place
-  const todosFromAPI = fetchTodoAPI()
-  const [todos] = React.useState(todosFromAPI)
+const TodoList = ({ todos }) => {
+	// ⛏️ Supprime (deplace dans App) l'appel API et le State Todos
+	// et utilise le props 'todos' à la place
+	// const todosFromAPI = fetchTodoAPI();
+	// const [todos] = React.useState(todosFromAPI);
 
-  return (
-    <div>
-      {todos.map(todo => (
-        <div key={todo.id}>{todo.name}</div>
-      ))}
-    </div>
-  )
-}
+	return <div>
+		{todos.map(todo => (
+			<div key={todo.id}>{todo.name}</div>
+		))}
+	</div>;
+};
 
-function App() {
-  // 🐶 Fais l'appel API pour récuperer les 'todos' et stocke le 
-  // dans une const 'todosFromAPI'
-  // 🐶 Créé un state 'todos' qui contient 'todosFromAPI' par defaut
+const App = () => {
+	// 🐶 Fais l'appel API pour récuperer les 'todos' et stocke le
+	// dans une const 'todosFromAPI'
+	// 🐶 Créé un state 'todos' qui contient 'todosFromAPI' par defaut
+	const todosFromAPI = fetchTodoAPI();
 
-  return (
-    <>
-      <Header />
-      <TodoList />
-    </>
-  )
-}
+	const [todos] = React.useState(todosFromAPI);
 
-export default App
+
+	return <>
+		<Header todos={todos} />
+		<TodoList todos={todos} />
+	</>;
+};
+
+export default App;
