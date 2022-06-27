8ca50653f16ef6e793d9968f8dad37ae4154ce19
Flo Slv <florent3969@hotmail.fr>
2022-06-27 16:41:17 +0200

    Exercice 2 done !

--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -1,32 +1,55 @@
 // Hook useEffect
 // http://localhost:3000/alone/exercise/02.js
 
-import * as React from 'react'
+import * as React from 'react';
 
 // 🐶 Corrige l'erreur grâce à useEffect
 
-function Login({initialEmail = ''}) {
-  const [email, setEmail] = React.useState(initialEmail)
-  const handleChange = async event => setEmail(event.target.value)
+const Login = ({ initialEmail = '' }) => {
+	const [email, setEmail] = React.useState(() => {
+		return window.localStorage.getItem('email') || initialEmail;
+	});
+	const [password, setPassword] = React.useState('');
 
-  // 🐶 Créé un Hook useEffect
-  // 🤖 React.useEffect(() => { ... })
+	const handleChange = async event => setEmail(event.target.value);
 
-  // ⛏️ Supprime ce code et insère le dans useEffect
-  console.log('Email Value', document.getElementById('email').value)
+	const handlePassChange = async e => setPassword(e.target.value);
 
-  return (
-    <div>
-      <form>
-        <label>Entrez votre email : </label>
-        <input id="email" value={email} onChange={handleChange} />
-      </form>
-    </div>
-  )
-}
+	// 🐶 Créé un Hook useEffect
+	// 🤖 React.useEffect(() => { ... })
+	React.useEffect(() => {
+		window.localStorage.setItem('email', email);
+		console.log('Email Value', document.getElementById('email').value);
+	}, [email]);
 
-function App() {
-  return <Login initialEmail="example@example.com" />
-}
+	React.useEffect(() => {
+		window.localStorage.setItem('email', initialEmail);
+		console.log('Email Value', document.getElementById('email').value);
+	}, [initialEmail]);
 
-export default App
+	return <div>
+		<form>
+			<label htmlFor={"email"}>Entrez votre email : </label>
+			<input id="email" value={email} onChange={handleChange} />
+
+			<label htmlFor="password">Entrez votre mot de passe : </label>
+			<input id="password" value={password} onChange={handlePassChange} />
+		</form>
+	</div>;
+};
+
+const App = () => {
+	const [count, setCount] = React.useState(0);
+
+	React.useEffect(() => {
+		const interval = setInterval(() => {
+			setCount(count => count + 1);
+		}, 5000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return <Login initialEmail={`example-${count}@example.com`} />;
+};
+
+export default App;
