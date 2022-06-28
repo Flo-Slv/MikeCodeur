b3c915c455a854165674fbc0a20b6ad9ebebefe6
Flo Slv <florent3969@hotmail.fr>
2022-06-28 14:53:41 +0200

    Exercice 5 done !

--- react-hooks/src/exercise/05.js
+++ react-hooks/src/exercise/05.js
@@ -1,32 +1,48 @@
 // useRef et useEffect
 // http://localhost:3000/alone/exercise/05.js
 
-import * as React from 'react'
+import * as React from 'react';
 // 🐶 importe confetti-js en décommentant la ligne ci-dessous
-//import ConfettiGenerator from 'confetti-js'
+import ConfettiGenerator from 'confetti-js';
 
-function Confetti() {
-  // 🐶 utilise useRef pour créer un référence 'confettiRef' vers le canvas de confetti
-  // 🤖 const confettiRef = React.useRef()
+const Confetti = () => {
+	// 🐶 utilise useRef pour créer un référence 'confettiRef' vers le canvas de confetti
+	// 🤖 const confettiRef = React.useRef()
+	const confettiRef = React.useRef();
 
-  // 🐶 utilise useEffect pour initiliser le générateur de confetti avec les bons paramètres
-  // 🤖 Initialisation :
-  // const confettiSettings = { target: ''}
-  // const confetti = new ConfettiGenerator(confettiSettings)
-  // confetti.render()
+	const refSetting = React.useRef({
+		clock: 50,
+		animate: Boolean(true),
+		max: 200
+	});
 
-  // 🐶 n'oublie pas de detruire l'objet confetti en retournant une fonction cleanup dans useEffect
-  // 🤖 return () => confetti.clear()
+	// 🐶 utilise useEffect pour initiliser le générateur de confetti avec les bons paramètres
+	// 🤖 Initialisation :
+	// const confettiSettings = { target: ''}
+	// const confetti = new ConfettiGenerator(confettiSettings)
+	// confetti.render()
+	React.useEffect(() => {
+		const confettiSettings = {
+			target: confettiRef.current,
+			...refSetting.current
+		};
+		const confetti = new ConfettiGenerator(confettiSettings);
+		confetti.render();
 
-  // 🐶 ajoute dans confettiSettings / target la référence de l'élement DOM canvas
-  // 🤖 const confettiSettings = { target: confettiRef.current}
+		return () => confetti.clear();
+	}, []);
 
-  // 🐶 ajoute la référence confettiRef au canvas
-  // 🤖 <canvas ref={confettiRef} />
-  return <canvas />
-}
+	// 🐶 n'oublie pas de detruire l'objet confetti en retournant une fonction cleanup dans useEffect
+	// 🤖 return () => confetti.clear()
 
-function App() {
-  return <Confetti />
-}
-export default App
+	// 🐶 ajoute dans confettiSettings / target la référence de l'élement DOM canvas
+	// 🤖 const confettiSettings = { target: confettiRef.current}
+
+	// 🐶 ajoute la référence confettiRef au canvas
+	// 🤖 <canvas ref={confettiRef} />
+	return <canvas ref={confettiRef} />;
+};
+
+const App = () => <Confetti />;
+
+export default App;
