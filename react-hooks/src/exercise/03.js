af076d5b8b234d23666fe2f4e072481463ac366b
Flo Slv <florent3969@hotmail.fr>
2022-06-28 11:26:48 +0200

    Exercice 3 part 1 done !

--- react-hooks/src/exercise/03.js
+++ react-hooks/src/exercise/03.js
@@ -1,59 +1,62 @@
 // Lifting state
 // http://localhost:3000/alone/exercise/03.js
 
-import * as React from 'react'
+import * as React from 'react';
 
 // 👨‍✈️ Nous voulons afficher dans le composant Content, l'ordinateur préféré
 
 // 🐶 accepte `computer` et `onComputerChange` en props de ce composant
-function MyBestComputer() {
-  // ⛏️ supprime ce state il sera géré dans le composant parent.
-  const [computer, setComputer] = React.useState('MacBookPro')
-  return (
-    <div>
-      <label>Mon ordinateur préféré : </label>
-      <input
-        value={computer}
-        // 🐶 remplace `setComputer` par `onComputerChange`
-        onChange={event => setComputer(event.target.value)}
-      />
-    </div>
-  )
-}
+const MyBestComputer = ({ computer, onComputerChange }) => {
+	// ⛏️ supprime ce state il sera géré dans le composant parent.
+	// const [computer, setComputer] = React.useState('MacBookPro');
 
-function UserName({userName, onUserNameChange}) {
-  return (
-    <div>
-      <label>Nom d'utilisateur : </label>
-      <input
-        value={userName}
-        onChange={event => onUserNameChange(event.target.value)}
-      />
-    </div>
-  )
-}
+	return <div>
+		<label>Mon ordinateur préféré : </label>
+		<input
+			value={computer}
+			// 🐶 remplace `setComputer` par `onComputerChange`
+			// onChange={event => setComputer(event.target.value)}
+			onChange={e => onComputerChange(e.target.value)}
+		/>
+	</div>;
+};
+
+const UserName = ({ userName, onUserNameChange }) => {
+	return <div>
+		<label>Nom d'utilisateur : </label>
+		<input
+			value={userName}
+			onChange={event => onUserNameChange(event.target.value)}
+		/>
+	</div>;
+};
 
 // 🐶 accepte `computer` en props
-function Content({userName}) {
-  return (
-    <div>
-      {/* ⛏️ surprime ce libellé */}
-      Salut <b>{userName}</b> !{/* 🐶 Utilise plutôt celui ci */}
-      {/* Salut <b>{userName}</b>, ton ordinateur préféré est <b>{computer}</b> */}
-    </div>
-  )
-}
-function App() {
-  // 🐶 ajoute useState 'computer'
-  const [userName, setUserName] = React.useState('')
-  return (
-    <div>
-      {/* 🐶 passe computer et onComputerChange en props */}
-      <MyBestComputer />
-      <UserName userName={userName} onUserNameChange={setUserName} />
-      {/* 🐶 passe le prop computer ici */}
-      <Content userName={userName} />
-    </div>
-  )
-}
-export default App
+const Content = ({ userName, computer }) => {
+	return <div>
+		{/* ⛏️ surprime ce libellé */}
+		{/*Salut <b>{userName}</b> ! 🐶 Utilise plutôt celui ci */}
+		{/* Salut <b>{userName}</b>, ton ordinateur préféré est <b>{computer}</b> */}
+		Salut <b>{userName}</b>, ton ordinateur pref est <b>{computer}</b>
+	</div>;
+};
+
+const App = () => {
+	// 🐶 ajoute useState 'computer'
+	const [userName, setUserName] = React.useState('');
+	const [computer, setComputer] = React.useState('PC');
+
+	const onComputerChange = comp => setComputer(comp);
+
+	return <div>
+		{/* 🐶 passe computer et onComputerChange en props */}
+		<MyBestComputer computer={computer} onComputerChange={onComputerChange} />
+
+		<UserName userName={userName} onUserNameChange={setUserName} />
+
+		{/* 🐶 passe le prop computer ici */}
+		<Content userName={userName} computer={computer}/>
+	</div>;
+};
+
+export default App;
