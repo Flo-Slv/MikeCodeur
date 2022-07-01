ffca489aacdab8379aae79e761170e0a952d7d86
Flo Slv <florent3969@hotmail.fr>
2022-07-01 10:50:59 +0200

    Exercice 3 done !

--- react-hooks-avances/src/exercise/03.js
+++ react-hooks-avances/src/exercise/03.js
@@ -1,146 +1,181 @@
 // Hook Perso
 // http://localhost:3000/alone/exercise/02.js
 
-import * as React from 'react'
-import {ErrorBoundary} from 'react-error-boundary'
+import * as React from 'react';
+import {ErrorBoundary} from 'react-error-boundary';
 import {
-  fetchMarvel,
-  fetchMarvelsList,
-  MarvelSearchForm,
-  ErrorDisplay,
-  MarvelPersoView,
-} from '../marvel'
-import '../02-styles.css'
+	fetchMarvel,
+	fetchMarvelsList,
+	MarvelSearchForm,
+	ErrorDisplay,
+	MarvelPersoView,
+} from '../marvel';
+
+import '../02-styles.css';
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case 'fetching':
-      return {status: 'fetching', data: null, error: null}
-    case 'done':
-      return {status: 'done', data: action.payload, error: null}
-    case 'fail':
-      return {status: 'fail', data: null, error: action.error}
-    default:
-      throw new Error('Action non supporté')
-  }
-}
+	switch (action.type) {
+		case 'fetching':
+			return {status: 'fetching', data: null, error: null}
+		case 'done':
+			return {status: 'done', data: action.payload, error: null}
+		case 'fail':
+			return {status: 'fail', data: null, error: action.error}
+		default:
+			throw new Error('Action non supporté')
+	}
+};
 
-// ⛏️ supprime 'search' il sera extrait dans 'useCallback' plus tard
-// 🐶 renomme 'fetch' en 'callback' pour plus de clarté
-function useFetchData(search, fetch) {
-  const [state, dispatch] = React.useReducer(reducer, {
-    data: null,
-    error: null,
-    status: 'idle',
-  })
+const useFetchData = fetch => {
+	const [state, dispatch] = React.useReducer(reducer, {
+		data: null,
+		error: null,
+		status: 'idle'
+	});
 
-  React.useEffect(() => {
-    // 🐶 fais un appel à la fonction 'callback' (qui retoune une 'promise')
-    // 🤖 const promise = callback()
+	const { data, error, status } = state;
 
-    // 🐶 sors de la fonction si  'promise' n'est pas défini
-    if (!search) {
-      return
-    }
-    dispatch({type: 'fetching'})
+	const execute = React.useCallback(promise => {
+		dispatch({ type: 'fetching' });
 
-    // ⛏️ supprime `fetch(search)` et utilise `promise`
-    fetch(search)
-      .then(marvel => dispatch({type: 'done', payload: marvel}))
-      .catch(error => dispatch({type: 'fail', error}))
-    // 🐶 adapte les dépendances pour que le useEffect ne s'excute sur la modification de 'callback'
-  }, [search, fetch])
+		promise
+			.then(res => dispatch({ type: 'done', payload: res }))
+			.catch(err => dispatch({ type: 'fail', error: err }));
+	}, []);
 
-  return state
-}
+	return { data, error, status, execute };
+};
 
-// 🐶 Modifie ce hook pour qu'il passe une fonction mémoïsé à 'useFetchData'
-function useFindMarvelList(marvelName) {
-  // 🐶 créé un callback avec `React.useCallback`
-  // 🤖 const cb = React.useCallback(param1, param2)
-  // 1. param1 est une fonction qui :
-  //    - retourne rien si 'marvelName' n'est pas défini
-  //    - return fetchMarvel(marvelName) si 'marvelName' n'est pas défini
-  // 2. param2 les dépendances (marvelName dans ce cas)
+const useFindMarvelList = marvelName => {
+	// const { data, error, status, execute } = useFetchData();
+	const [data, error, status, execute] = useFetchData();
 
-  // ⛏️ supprime le paramètre 'marvelName'
-  // 🐶 passe le callback à 'useFetchData'
-  return useFetchData(marvelName, fetchMarvelsList)
-}
+	React.useEffect(() => {
+		if (!marvelName) return;
 
-// 🐶 Modifie ce hook pour qu'il passe une fonction mémoïsé à 'useFetchData'
-function useFindMarvelByName(marvelName) {
-  // 🐶 réptète l'opération
-  return useFetchData(marvelName, fetchMarvel)
-}
+		execute(fetchMarvelsList(marvelName));
+	}, [marvelName, execute]);
 
-function Marvel({marvelName}) {
-  const state = useFindMarvelByName(marvelName)
-  const {data: marvel, error, status} = state
-  if (status === 'fail') {
-    throw error
-  } else if (status === 'idle') {
-    return 'enter un nom de Marvel'
-  } else if (status === 'fetching') {
-    return 'chargement en cours ...'
-  } else if (status === 'done') {
-    return <MarvelPersoView marvel={marvel} />
-  }
-}
+	return { data, error, status };
+};
 
-function MarvelList({marvelName}) {
-  const state = useFindMarvelList(marvelName)
-  const {data: marvels, error, status} = state
-  if (status === 'fail') {
-    throw error
-  } else if (status === 'idle') {
-    return 'enter un nom de Marvel'
-  } else if (status === 'fetching') {
-    return 'chargement en cours ...'
-  } else if (status === 'done') {
-    return (
-      <>
-        {marvels.map(marvel => {
-          return (
-            <div key={marvel.id}>
-              <hr style={{background: 'grey'}} />
-              <MarvelPersoView marvel={marvel} />
-            </div>
-          )
-        })}
-      </>
-    )
-  }
-}
+const useFindMarvelByName = marvelName => {
+	const { data, error, status, execute } = useFetchData();
 
-function App() {
-  const [marvelName, setMarvelName] = React.useState('')
-  const [searchList, setSearchList] = React.useState('')
-  const handleSearch = name => {
-    setMarvelName(name)
-  }
-  return (
-    <div className="marvel-app">
-      <label>
-        <input
-          type="checkbox"
-          checked={searchList}
-          onChange={e => setSearchList(e.target.checked)}
-        />{' '}
-        Chercher une liste ?
-      </label>
-      <MarvelSearchForm marvelName={marvelName} onSearch={handleSearch} />
-      <div className="marvel-detail">
-        <ErrorBoundary key={marvelName} FallbackComponent={ErrorDisplay}>
-          {searchList ? (
-            <MarvelList marvelName={marvelName} />
-          ) : (
-            <Marvel marvelName={marvelName} />
-          )}
-        </ErrorBoundary>
-      </div>
-    </div>
-  )
-}
+	React.useEffect(() => {
+		if (!marvelName) return;
 
-export default App
+		execute(fetchMarvel(marvelName));
+	}, [marvelName, execute]);
+
+	return { data, error, status };
+};
+
+// const useFetchData = callback => {
+// 	const [state, dispatch] = React.useReducer(reducer, {
+// 		data: null,
+// 		error: null,
+// 		status: 'idle'
+// 	});
+
+// 	React.useEffect(() => {
+// 		const promise = callback();
+
+// 		if (!promise) return;
+
+// 		dispatch({type: 'fetching'});
+
+// 		promise
+// 			.then(marvel => dispatch({type: 'done', payload: marvel}))
+// 			.catch(error => dispatch({type: 'fail', error}))
+// 	}, [callback]);
+
+// 	return state;
+// };
+
+// const useFindMarvelList = marvelName => {
+// 	const cb = React.useCallback(() => {
+// 		if (!marvelName) return;
+
+// 		return fetchMarvelsList(marvelName);
+// 	}, [marvelName]);
+
+// 	return useFetchData(cb);
+// };
+
+// const useFindMarvelByName = marvelName => {
+// 	const cb = React.useCallback(() => {
+// 		if (!marvelName) return;
+
+// 		return fetchMarvel(marvelName);
+// 	}, [marvelName]);
+
+// 	return useFetchData(cb);
+// };
+
+const Marvel = ({ marvelName }) => {
+	const state = useFindMarvelByName(marvelName)
+	const {data: marvel, error, status} = state
+	if (status === 'fail') {
+		throw error
+	} else if (status === 'idle') {
+		return 'enter un nom de Marvel'
+	} else if (status === 'fetching') {
+		return 'chargement en cours ...'
+	} else if (status === 'done') {
+		return <MarvelPersoView marvel={marvel} />
+	}
+};
+
+const MarvelList = ({ marvelName }) => {
+	const state = useFindMarvelList(marvelName)
+	const {data: marvels, error, status} = state
+
+	if (status === 'fail') throw error
+
+	else if (status === 'idle')
+		return 'enter un nom de Marvel'
+	else if (status === 'fetching')
+		return 'chargement en cours ...'
+	 else if (status === 'done')
+		return <>
+			{marvels.map(marvel => {
+				return <div key={marvel.id}>
+					<hr style={{background: 'grey'}} />
+					<MarvelPersoView marvel={marvel} />
+				</div>;
+			})}
+		</>;
+};
+
+const App = () => {
+	const [marvelName, setMarvelName] = React.useState('');
+	const [searchList, setSearchList] = React.useState('');
+
+	const handleSearch = name => setMarvelName(name);
+
+	return <div className="marvel-app">
+		<label>
+			<input
+				type="checkbox"
+				checked={searchList}
+				onChange={e => setSearchList(e.target.checked)}
+			/>{' '}
+			Chercher une liste ?
+		</label>
+
+		<MarvelSearchForm marvelName={marvelName} onSearch={handleSearch} />
+
+		<div className="marvel-detail">
+			<ErrorBoundary key={marvelName} FallbackComponent={ErrorDisplay}>
+				{searchList ? (
+					<MarvelList marvelName={marvelName} />
+				) : (
+					<Marvel marvelName={marvelName} />
+				)}
+			</ErrorBoundary>
+		</div>
+	</div>;
+};
+
+export default App;
